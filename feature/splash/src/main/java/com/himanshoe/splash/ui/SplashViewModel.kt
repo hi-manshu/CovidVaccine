582e3c70f401ac14b31be754ff1cffa939d32b22
Himanshu Singh <hello2himanshusingh@gmail.com>
2021-04-30 15:40:33 +0530

    Session Management done

--- feature/splash/src/main/java/com/himanshoe/splash/ui/SplashViewModel.kt
+++ feature/splash/src/main/java/com/himanshoe/splash/ui/SplashViewModel.kt
@@ -3,7 +3,9 @@ package com.himanshoe.splash.ui
 import androidx.lifecycle.viewModelScope
 import com.himanshoe.core.base.BaseViewModel
 import com.himanshoe.core.navigation.Navigator
+import com.himanshoe.core.storage.session.SessionManager
 import com.himanshoe.core.util.NetworkHelper
+import com.himanshoe.splash.util.deepLinkToDashboard
 import com.himanshoe.splash.util.deepLinkToState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
@@ -11,7 +13,10 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class SplashViewModel @Inject constructor(networkHelper: NetworkHelper) :
+class SplashViewModel @Inject constructor(
+    networkHelper: NetworkHelper,
+    private val sessionManager: SessionManager
+) :
     BaseViewModel(networkHelper) {
 
     val navigator = Navigator()
@@ -19,7 +24,11 @@ class SplashViewModel @Inject constructor(networkHelper: NetworkHelper) :
     fun init() {
         viewModelScope.launch {
             delay(2000)
-            navigator.navigate(deepLinkToState())
+            if (sessionManager.isOnboardingDone()) {
+                navigator.navigate(deepLinkToDashboard())
+            } else {
+                navigator.navigate(deepLinkToState())
+            }
         }
     }
 }
